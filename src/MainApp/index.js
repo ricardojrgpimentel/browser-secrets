1ca30c3b87809817b2bc292edd4bf4b7b17fbac2
Ricardo Pimentel <ricardojrgpimentel@gmail.com>
2017-09-22 17:17:29 +0100

    added info

--- src/MainApp/index.js
+++ src/MainApp/index.js
@@ -2,9 +2,15 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap-grid.min.css'
 import 'bootstrap/dist/css/bootstrap-reboot.min.css'
 import './style.css'
+import parser from 'ua-parser-js'
 
 class MainApp extends React.Component{
+
 	render(){
+		
+		let ua = parser()
+		console.log(ua)
+
 		return(
 			<div className='main-wrap'>
 				<div className="section1">
@@ -13,7 +19,24 @@ class MainApp extends React.Component{
 						<p className="message">Everything the internet knows about you from your Browser</p>
 					</div>
 				</div>
-				
+				<div className="section2">
+					<div className="container">
+						<p>Browser: {ua.browser.name}</p>
+						<p>Browser: {ua.browser.version}</p>
+						<p>CPU: {ua.cpu.architecture}</p>
+						<p>Device: {ua.device.model}</p>
+						<p>Device: {ua.device.type}</p>
+						<p>Device: {ua.device.vendor}</p>
+						<p>OS: {ua.os.name}</p>
+						<p>OS: {ua.os.version}</p>
+						{console.log(navigator)}
+						{/* navigator.plugins.map((plugin, idx) => {
+							return(
+								<p>{plugin.name}</p>
+							)
+						}) */}
+					</div>
+				</div>
 			</div>
 		)
 	}
